24004231fc9433bdde572f3faffb815f6c8c075f
Platforma <platformaQualitetMarket@gmail.com>
2026-07-13 13:25:13 +0200

    Update root route to serve frontend/index.html

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/listing.html"));
+  res.sendFile(path.join(__dirname, "frontend/index.html"));
 });
 
 app.get("/api/products", (req, res) => {
